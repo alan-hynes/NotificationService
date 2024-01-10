ede2c6f47882ddc7714ad59a0969e30f30112974
alan-hynes <G00400498@atu.ie>
2024-01-10 17:58:31 +0000

    created and populated NotificationController class

---
+++ src/main/java/ie/atu/notificationservice/NotificationController.java
@@ -0,0 +1,21 @@
+package ie.atu.notificationservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    @Autowired
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @PostMapping("/notifications")
+    public void createNotification(@RequestBody NotificationRequest request) {
+        notificationService.sendNotification(request);
+    }
+}
